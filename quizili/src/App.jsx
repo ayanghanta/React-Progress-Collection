04aa7778e2574b25b987570bd575d77bf20fc62d
ayanghanta <ayanghanta674@gmail.com>
2024-09-15 17:37:23 +0530

    refactor using uesReducer+context API

--- quizili/src/App.jsx
+++ quizili/src/App.jsx
@@ -1,18 +1,51 @@
-import Main from "./components/Main";
+import MainSection from "./components/MainSection";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Topics from "./components/Topics";
+import Loader from "./components/Loader";
+import Question from "./components/Question";
+import ScoreBord from "./components/ScoreBord";
+import StartQuiz from "./components/StartQuiz";
+import Herosection from "./components/HeroSection";
+import SelectQuantity from "./components/SelectQuantity";
+import SelecetDificulity from "./components/SelecetDificulity";
 
-import { QuizProvider } from "./context/QuizContext";
+import { useQuiz } from "./context/QuizContext";
 
 // choose[topic,quantity,dificulity], loading/quation, quiz[ready,active.finish]
 
 function App() {
+  const { stage } = useQuiz();
   return (
     <div className="container">
       <Header />
-      <QuizProvider>
-        <Main></Main>
-      </QuizProvider>
+
+      <MainSection>
+        <div className="mainConatiner">
+          {stage == "choose/topic" && (
+            <>
+              <Herosection />
+              <Topics />
+            </>
+          )}
+          {stage == "choose/quantity" && (
+            <>
+              <Herosection />
+              <SelectQuantity />
+            </>
+          )}
+          {stage == "choose/difficulity" && (
+            <>
+              <Herosection />
+              <SelecetDificulity />
+            </>
+          )}
+          {stage == "loading/questions" && <Loader />}
+          {stage == "quiz/ready" && <StartQuiz />}
+          {stage === "quiz/active" && <Question />}
+          {stage === "quiz/finish" && <ScoreBord />}
+        </div>
+      </MainSection>
 
       <Footer />
     </div>
---
+++ quizili/src/components/MainSection.jsx
@@ -0,0 +1,5 @@
+function MainSection({ children }) {
+  return <div>{children}</div>;
+}
+
+export default MainSection;
